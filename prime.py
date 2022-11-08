e3521defd975ea8bbe819a8c3f400ce108e65d1d
Mansi Jain <95686383+mansij27@users.noreply.github.com>
2022-11-08 15:24:41 +0530

    ai

--- prime.py
+++ prime.py
@@ -1,9 +1,13 @@
-num= int(input("Enter any number"))
-i=2
-while range(2,num):
-    if num % i==0:
-        print("Not Prime")
-        break
-    else:
-        print("Prime")
-        break
+num= int(input("Enter any number: "))
+flag=False
+
+if num>1:
+    for i in range(2,num):
+        if (num % i)==0:
+            flag=True
+            break
+
+if flag:
+    print(num, 'is Not Prime number')
+else:
+    print(num , "is Prime number")
